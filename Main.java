57bac652846620a3ccf9aacbe912b0983977066e
cmagnus <cmagnus@gmail.com>
2016-11-21 15:44:22 -0500

    refactored class names. still need to modify further to make into trees

---
+++ Main.java
@@ -0,0 +1,11 @@
+/* Main.java
+ * written by csi/cen 213 students fall 2016
+ * 
+ * A class for learning about trees.
+ */
+public class LinkedLists {
+  public static void main (String [] args) {
+    Tree myTree = new Tree();
+    
+  }
+}
